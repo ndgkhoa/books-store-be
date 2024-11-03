888af8cf371a1e9a67ae02cfde51645bf7bfeb9e
khoa <user.iamjasper@gmail.com>
2024-11-04 02:18:37 +0700

    CRUD book

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
+const bookRoutes = require('./src/books/book.route')
+
 const app = express()
 const port = process.env.PORT || 5000
-
 require('dotenv').config()
+
+app.use(express.json())
+app.use(
+    cors({
+        origin: ['http://localhost:5173'],
+        credentials: true,
+    }),
+)
+
+app.use('/api/books', bookRoutes)
 
 async function main() {
     await mongoose.connect(process.env.MONGO_URL)
---
+++ src/books/book.route.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const {
+    createBook,
+    getAllBooks,
+    getSingleBook,
+    updateBook,
+    deleteBook,
+} = require('./book.controller')
+const router = express.Router()
+
+router.post('/create-book', createBook)
+router.get('/', getAllBooks)
+router.get('/:id', getSingleBook)
+router.put('/edit/:id', updateBook)
+router.delete('/:id', deleteBook)
+
+module.exports = router
