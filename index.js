778447527afe5a1370c226b6a531fb654a2a03a5
khoa <user.iamjasper@gmail.com>
2024-11-05 05:25:07 +0700

    feat: create order

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const bookRoutes = require('./src/books/book.route')
+const orderRoutes = require('./src/orders/order.route')
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -16,6 +17,7 @@ app.use(
 )
 
 app.use('/api/books', bookRoutes)
+app.use('/api/orders', orderRoutes)
 
 async function main() {
     await mongoose.connect(process.env.MONGO_URL)
