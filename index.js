1a5576f7f63102576c645cad8439373ba7cf8f8c
khoa <user.iamjasper@gmail.com>
2024-11-06 17:26:34 +0700

    feat: get dashboard stat

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const bookRoutes = require('./src/books/book.route')
 const orderRoutes = require('./src/orders/order.route')
 const userRoutes = require('./src/users/user.route')
+const adminRoutes = require('./src/stats/admin.stats')
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -12,7 +13,7 @@ require('dotenv').config()
 app.use(express.json())
 app.use(
     cors({
-        origin: ['http://localhost:5173'],
+        origin: ['http://localhost:5173', 'https://books-store-nu.vercel.app'],
         credentials: true,
     }),
 )
@@ -20,17 +21,18 @@ app.use(
 app.use('/api/books', bookRoutes)
 app.use('/api/orders', orderRoutes)
 app.use('/api/auth', userRoutes)
+app.use('/api/admin', adminRoutes)
+
+app.get('/', (req, res) => {
+    res.send('Server is running!')
+})
 
 async function main() {
     await mongoose.connect(process.env.MONGO_URL)
-    app.get('/', (req, res) => {
-        res.send('Hello World!')
-    })
+    console.log('Mongodb connect successfully')
 }
 
-main()
-    .then(() => console.log('Mongodb connect successfully'))
-    .catch((err) => console.log(err))
+main().catch((err) => console.log(err))
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
